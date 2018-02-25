dd2d1aa226747cd62d170d52a5a4384c805d20be
Tiago Matos <tiago@tiagomatos.com>
2018-02-25 11:12:14 +1100

    Update eslint rule with identation

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App),
+    el: '#app',
+    router,
+    render: h => h(App),
 });
